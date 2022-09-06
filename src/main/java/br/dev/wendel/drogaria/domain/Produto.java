057b479130da9e1a3f68556c66a97a71c0ff89d2
WendelSantosD <wendelwcsantos@gmail.com>
2022-09-05 23:53:56 -0300

    feat: init config entity with lombok

--- src/main/java/br/dev/wendel/drogaria/domain/Produto.java
+++ src/main/java/br/dev/wendel/drogaria/domain/Produto.java
@@ -3,7 +3,14 @@ package br.dev.wendel.drogaria.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
 	private Short codigo;
 	
